ddf8bb687003938c64d6e0a38c546a11f22334da
Marco <marcotruffat@paucasesnovescifp.cat>
2020-10-15 20:13:40 +0200

    Exercise 8 finished.
    Added a listBox, 2 buttons for every classroom and a reset button to clear the list. The selected item in listbox will be copied to the textbox.

--- BasicControlsWinForms_Exercise8/Form1.cs
+++ BasicControlsWinForms_Exercise8/Form1.cs
@@ -16,5 +16,34 @@ namespace BasicControlsWinForms_Exercise8
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Insert(0, "Rian Hurst");
+            listBox1.Items.Insert(1, "Kimberly Corbett");
+            listBox1.Items.Insert(2, "Maaria Ridley");
+            listBox1.Items.Insert(3, "Antonina Rennie");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Insert(0, "Zach Steadman");
+            listBox1.Items.Insert(1, "Tiffany Benton");
+            listBox1.Items.Insert(2, "Jadine Mason");
+            listBox1.Items.Insert(3, "Aaliya Burton");
+            listBox1.Items.Insert(4, "Ariya Holcomb");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = listBox1.GetItemText(listBox1.SelectedItem);
+        }
     }
 }
